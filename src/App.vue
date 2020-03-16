94c897417df07866d52b1cbb1a49dec4418d0cb4
Neelansh Mathur <neelansh15@outlook.com>
2020-03-16 11:06:37 +0530

    Play and Pause and selecting new songs work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>My Music</h1>
+    </header>
+    <main>
+      <section class="players">
+
+        <h2 class="song-title">
+          {{ current.title }}
+          <span> - By {{ current.artist }}</span>
+        </h2>
+
+        <div class="controls">
+          <button class="prev">Previous</button>
+          <button class="play" v-if="!isPlaying" v-on:click="play">Play</button>
+          <button class="pause" v-else v-on:click="pause">Pause</button>
+          <button class="next">Next</button>
+        </div>
+
+      </section>
+
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{ song.title }}
+        </button>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return {
+      current:{
+        
+      },
+      index: 0,
+      isPlaying: false,
+      songs:[
+        {
+          title: 'Broken',
+          artist: 'Neelansh',
+          src: require('./assets/Broken.mp3')
+        },
+        {
+          title: 'Show your Mettle',
+          artist: 'Neelansh',
+          src: require('./assets/Mettle.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+
+  methods:{
+    play(song){
+      if (typeof song.src != "undefined"){
+        this.current = song
+        this.player.src = this.current.src
+      }
+      this.player.play()
+      this.isPlaying = true
+    },
+
+    pause(){
+      this.player.pause()
+      this.isPlaying = false
+    }
+  },
+
+  created(){
+    this.current = this.songs[this.index]
+    this.player.src = this.current.src
+    // this.player.play()
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: sans-serif;
+}
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  color: white;
+  background: #212121;
+}
+span{
+  color: #af2d2d;
 }
 </style>
