29f31480fa79014a5184ddffa831a81a3eab1b5a
Neelansh Mathur <neelansh15@outlook.com>
2020-03-16 11:34:34 +0530

    Everything works now! CSS Done too

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,18 @@ export default {
         this.player.src = this.current.src
       }
       this.player.play()
+
+      //Question: Why not just call the next() method directly??
+      this.player.addEventListener('ended', function(){
+        this.index++
+        
+        if(this.index > this.songs.length - 1)
+          this.index = 0
+      
+        this.current = this.songs[this.index]
+        this.play(this.current)
+      }.bind(this))
+
       this.isPlaying = true
     },
 
@@ -115,7 +127,91 @@ header{
   color: white;
   background: #212121;
 }
-span{
-  color: #af2d2d;
+main{
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
 }
+
+.song-title{
+  color: #212121;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span{
+  font-weight: 400;
+  font-style: italic;
+  color: #962121;
+}
+
+.controls{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+
+button{
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+
+button:hover{
+  opacity: 0.8;
+}
+
+.play, .pause{
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: white;
+  background-color: rgb(219, 39, 78);
+}
+
+.next, .prev{
+  font-size: 16px;
+  font-weight: 700;
+  padding: 12px 20px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: white;
+  background-color: rgb(185, 32, 65);
+}
+
+.playlist{
+  padding: 0px 30px;
+}
+.playlist h3{
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist .song{
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.playlist .song:hover{
+  color: #FF5858;
+}
+
+.playlist .song.playing{
+  color: white;
+  background: linear-gradient(to right, #CC2E5D, #FF5858);
+}
+
 </style>
